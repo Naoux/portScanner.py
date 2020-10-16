a4d3c2b1295928ae81ff3176ae93179dbc269bea
Naoux <naoux.sh@outlook.com>
2020-10-16 15:23:02 +0200

    scannerPort
    J'ai réalisé un scanner de port en python. Il n'est pas fini a 100% mais est fonctionnel. 
    Usages: python scannerPort.py <domaine> <PORT>
                 python scannerPort.py <domaine> <PORTmin> <PORTmax>

--- scannerPort.py
+++ scannerPort.py
@@ -1,61 +1,76 @@
 import socket
 import sys
 
-
-HOST = sys.argv[1]
-PORTmin = sys.argv[2]
 portOuvert = []
 portFerme = []
 PORT = []
 
-if len(sys.argv) == 4:
+if len(sys.argv) == 3 or len(sys.argv) == 4:
 	try:
-		PORTmax = int(sys.argv[3])
+		HOST = sys.argv[1]
 	except:
-		print("[-] Port invalide")
+		print("[-] Domaine invalide")
+		sys.exit()
 
-try:
-	PORTmin = int(PORTmin)
+	try:
+		PORTmin = sys.argv[2]
+	except:
+		print("[-] Port invalide.")
+		sys.exit()
+
 	if len(sys.argv) == 4:
-		for n in range(PORTmin, PORTmax):
-			PORT.append(n)
-			n += 1
-	else:
-		PORT.append(PORTmin)
-except ValueError:
-	print("[-] Port invalide")
-
-i = 0
-while i < len(PORT[:]):
-	try:
-		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	except socket.error as e:
-		print("[-] Une erreur est survenue lors de la creation du socket ! {}".format(e))
-		sys.exit()
+		try:
+			PORTmax = int(sys.argv[3])
+		except:
+			print("[-] Port invalide")
+			sys.exit()
 
 	try:
-		scan = sock.connect_ex((HOST, PORT[i]))
-		if scan == 0:
-			sock.shutdown(socket.SHUT_RDWR)
-			sock.close()
-			portOuvert.append(PORT[i])
+		PORTmin = int(PORTmin)
+		if len(sys.argv) == 4:
+			for n in range(PORTmin, PORTmax):
+				PORT.append(n)
+				n += 1
 		else:
-			portFerme.append(PORT[i])
-		i += 1
-	except socket.gaierror as e:
-		print("[-] Erreur d'adresse de connexion au serveur: {}".format(e))
-		sys.exit()
-	except socket.error as e:
-		print("[-] Erreur de connexion : {}".format(e))
+			PORT.append(PORTmin)
+	except ValueError:
+		print("[-] Port invalide")
 		sys.exit()
 
-if len(portOuvert) == 0:
-	print("[-] Tous les ports sont fermes !")
+	i = 0
+	while i < len(PORT[:]):
+		try:
+			sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		except socket.error as e:
+			print("[-] Une erreur est survenue lors de la creation du socket ! {}".format(e))
+			sys.exit()
+
+		try:
+			scan = sock.connect_ex((HOST, PORT[i]))
+			if scan == 0:
+				sock.shutdown(socket.SHUT_RDWR)
+				sock.close()
+				portOuvert.append(PORT[i])
+			else:
+				portFerme.append(PORT[i])
+			i += 1
+		except socket.gaierror as e:
+			print("[-] Erreur d'adresse de connexion au serveur: {}".format(e))
+			sys.exit()
+		except socket.error as e:
+			print("[-] Erreur de connexion : {}".format(e))
+			sys.exit()
+
+	if len(portOuvert) == 0:
+		print("[-] Tous les ports sont fermes !")
+	else:
+		for port in portOuvert:
+			print("[+] Port {} ouvert".format(port))
+		for port in portFerme:
+			print("[-] Port {} ferme".format(port))
 else:
-	for port in portOuvert:
-		print("[+] Port {} ouvert".format(port))
-	for port in portFerme:
-		print("[-] Port {} ferme".format(port))
+	print("Usages: python scannerPort.py <domaine> <PORT>\n",
+		  "\tpython scannerPort.py <domaine> <PORTmin> <PORTmax>")
 
 
 
